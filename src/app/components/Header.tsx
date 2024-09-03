41bdd6f2c36930f364bf8af50cae82b47ab8e168
anton-glebov <anton.glebov@useinsider.com>
2024-09-03 16:56:22 +0300

    fix havigation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,12 @@
-import { Link } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
+import { useLocation } from 'react-router-dom';
 import { linksData } from '@/app/data';
 
 export default function Header() {
-  const { hash } = window.location;
-  const { pathname } = window.location;
+  const { hash, pathname } = useLocation();
 
-  function isActive(linkHash: string): boolean {
-    if (pathname === 'about') {
-      return false;
-    }
-
-    return linkHash === hash;
+  function isActive(page: string, linkHash: string): boolean {
+    return page === pathname && linkHash === hash;
   }
 
   return (
@@ -23,18 +19,18 @@ export default function Header() {
         className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0"
       >
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-          {linksData.map(({ name, hash, title }, index) => (
+          {linksData.map(({ page, hash, title }, index) => (
             <li
               key={`link-${index}`}
               className="h-3/4 flex items-center justify-center relative"
               style={{ opacity: 1, transform: 'none' }}
             >
               <Link
-                to={`${name}${hash}`}
+                to={`${page}${hash}`}
                 className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300 text-gray-950 dark:text-gray-200"
               >
                 {title}
-                {isActive(hash) && (
+                {isActive(page, hash) && (
                 <span
                   className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
                   style={{ transform: 'none', transformOrigin: '50% 50% 0' }}
